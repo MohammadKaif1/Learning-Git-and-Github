9899df3dfa3492b6b1ddea0f011520f1a70d2b65
Mohammad Kaif <kaif1695@gmail.com>
2022-07-26 17:06:29 +0530

    adding diff.java

---
+++ Diff.java
@@ -0,0 +1,9 @@
+package Extra;
+
+public class Diff {
+    public static void main(String[] args) {
+        int a= 50;
+        int b=90;
+        System.out.print("The difference : "+(b-a));
+    }
+}
